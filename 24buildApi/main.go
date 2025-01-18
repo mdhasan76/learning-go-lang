bdf81d3b233b90026450ef22c0e46e66c3d9321b
mdhasan76 <mdhasan8064@gmail.com>
2025-01-18 22:13:16 +0600

    design data model for course

--- 24buildApi/main.go
+++ 24buildApi/main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+type Course struct {
+	CourseId    string  `json:"courseId"`
+	CourseName  string  `json:"courseName"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	FullName string `json:"fullName"`
+	Website  string `json:"website"`
+}
+
+// fake db
+var courses []Course
+
+// middleware, helper fn
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
 func main() {
 	fmt.Println("Hello")
 }
