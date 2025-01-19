3c20015da540aec07889f0a67fb8227a1f12e5d7
mdhasan76 <mdhasan8064@gmail.com>
2025-01-19 11:44:53 +0600

    create one and get one course controllerfn

--- 24buildApi/main.go
+++ 24buildApi/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type Course struct {
@@ -40,4 +45,44 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// garb id from request url
+	params := mux.Vars(r)
+
+	// loop throw courses to find by id and return in response
+	for _, course := range courses {
+		if course.CourseId == params["courseId"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No Courses found with the given id")
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course ")
+	w.Header().Set("Content-Type", "application/json")
+
+	// what if: body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send with some data")
+	}
+
+	// what about - {}
+	var course Course
+	_ = json.NewDecoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data inside json")
+		return
+	}
+
+	// generate random number
+	rand.NewSource(time.Now().UnixNano())
+	course.CourseId = strconv.Itoa(rand.Intn(100))
 }
