852b0ea8992a196b003c9bda40345c161db6bddb
mdhasan76 <mdhasan8064@gmail.com>
2025-01-20 21:15:52 +0600

    add db sting

---
+++ 25mongoAPI/controller/controller.go
@@ -0,0 +1,7 @@
+package controller
+
+// const connectionString = "mongodb+srv://shohag_isp:oA1zfB43JZkFLHqx@cluster0.di4ojvf.mongodb.net/"
+const connectionString = "mongodb+srv://go_netflix:lW28H9FzpWhDA2gJ@cluster0.mj4ed9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+// id: go_netflix
+// pass: lW28H9FzpWhDA2gJ
