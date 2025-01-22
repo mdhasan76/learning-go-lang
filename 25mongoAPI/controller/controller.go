5034a7d8736bde62610bf18eaef859d3e7e88400
mdhasan76 <mdhasan8064@gmail.com>
2025-01-22 15:19:35 +0600

    created controller fn for add, delete one, and delete many movie from collection

--- 25mongoAPI/controller/controller.go
+++ 25mongoAPI/controller/controller.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"mongoAPI/model"
 
-	"go.mongodb.org/mongo-driver/v2/mongo"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
 )
 
 // const connectionString = "mongodb+srv://shohag_isp:oA1zfB43JZkFLHqx@cluster0.di4ojvf.mongodb.net/"
@@ -16,7 +20,7 @@ const dbName = "netflix"
 const collectionName = "watchlist"
 
 // most important
-var collection *mongo.collection
+var collection *mongo.Collection
 
 // Connect with db
 func init() {
@@ -35,4 +39,53 @@ func init() {
 
 	// Collection instance
 	fmt.Println("Collection instance is ready")
+}
+
+// Mongodb helper  - file
+
+// insert 1 record
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted 1 movie in db with id:", inserted.InsertedID)
+}
+
+func updateOneMovie(movieId string) {
+	id, err := primitive.ObjectIDFromHex(movieId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Modified Count:", result.ModifiedCount)
+}
+
+func deleteOneMovie(movieId string) {
+	id, _ := primitive.ObjectIDFromHex(movieId)
+	filter := bson.M{"_id": id}
+
+	result, err := collection.DeleteOne(context.Background(), filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Movie is deleted successfully", result)
+}
+
+func deleteAllMovie() int64 {
+	result, err := collection.DeleteMany(context.Background(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Deleted all Movie successfully. Total deleted count is: ", result.DeletedCount, nil)
+	return result.DeletedCount
 }
